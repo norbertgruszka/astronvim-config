8380f6f18372e7734a3d028f5391fb733fe767a3
Norbert Gruszka <ngru95@outlook.com>
2023-04-14 16:05:20 +0200

    Improve key mappings

--- mappings.lua
+++ mappings.lua
@@ -4,25 +4,35 @@
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
 return {
-  -- first key is the mode
-  n = {
-    -- second key is the lefthand side of the map
-    -- mappings seen under group name "Buffer"
-    -- ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    -- ["<leader>bD"] = {
-    --   function()
-    --     require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
-    --   end,
-    --   desc = "Pick to close",
-    -- },
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
-    -- ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+ -- first key is the mode
+ n = {
+  -- second key is the lefthand side of the map
+  -- mappings seen under group name "Buffer"
+  -- ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+  -- ["<leader>bD"] = {
+  --   function()
+  --     require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+  --   end,
+  --   desc = "Pick to close",
+  -- },
+  -- tables with the `name` key will be registered with which-key if it's installed
+  -- this is useful for naming menus
+  -- ["<leader>b"] = { name = "Buffers" },
+  -- quick save
+  -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+  ["<S-l>"] = { ":bnext<CR>", desc = "Switch to next buffer" },
+  ["<S-h>"] = { ":bprevious<CR>", desc = "Switch to previous buffer" },
  },
-  t = {
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
-  },
+ t = {
+  -- setting a mapping to false will disable it
+  -- ["<esc>"] = false,
+ },
+ v = {
+  ["<"] = { "<gv", desc = "Intend text." },
+  [">"] = { ">gv", desc = "Revers Intend text." },
+ },
+ x = {
+  ["<A-j>"] = { ":move '>+1<CR>gv-gv", desc = "Move text up" },
+  ["<A-k>"] = { ":move '<-2<CR>gv-gv", desc = "Move down up" }
+ }
 }
